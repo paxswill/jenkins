7f6b64007380d69262e80ab5fe664ba93b28c3ca
Will Ross <wross@cmf.nrl.navy.mil>
2011-07-11 12:08:27 -0400

    Preference Pane Project

---
+++ osx/PrefPane/Jenkins/Jenkins.h
@@ -0,0 +1,15 @@
+//
+//  Jenkins.h
+//  Jenkins
+//
+//  Created by William Ross on 7/11/11.
+//  Copyright 2011 Naval Research Lab. All rights reserved.
+//
+
+#import <PreferencePanes/PreferencePanes.h>
+
+@interface Jenkins : NSPreferencePane
+
+- (void)mainViewDidLoad;
+
+@end
---
+++ osx/PrefPane/Jenkins/Jenkins.m
@@ -0,0 +1,17 @@
+//
+//  Jenkins.m
+//  Jenkins
+//
+//  Created by William Ross on 7/11/11.
+//  Copyright 2011 Naval Research Lab. All rights reserved.
+//
+
+#import "Jenkins.h"
+
+@implementation Jenkins
+
+- (void)mainViewDidLoad
+{
+}
+
+@end
