bd6bda672fca6bd1522e152174e132d5458a07ff
Will Ross <wross@cmf.nrl.navy.mil>
2011-07-12 16:35:39 -0400

    FILE * vs fd confiusion

--- osx/PrefPane/SecureWrite/main.m
+++ osx/PrefPane/SecureWrite/main.m
@@ -21,17 +21,18 @@ int main (int argc, const char * argv[])
 		exit(1);
 	}
 	// The pipe handle should already be open
+	int readFD = fcntl(STDIN_FILENO, F_DUPFD, 0);
 	int writeFD = open(argv[2], O_WRONLY | O_CREAT);
 	assert(writeFD > 0);
 	size_t bufferSize = 1024;
 	size_t actualSize;
 	void *buffer = malloc(bufferSize);
-	actualSize = read(stdin, buffer, bufferSize);
+	actualSize = read(readFD, buffer, bufferSize);
 	while(actualSize != 0){
 		assert(actualSize > 0);
 		ssize_t writtenBytes = write(writeFD, buffer, actualSize);
 		assert(writtenBytes > 0);
-		actualSize = read(stdin, buffer, bufferSize);
+		actualSize = read(readFD, buffer, bufferSize);
 	}
 	free(buffer);
 	close(writeFD);
