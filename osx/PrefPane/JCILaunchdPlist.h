3f05e1c90f3e94e4820bf5be0475efa01a10af2f
Will Ross <wross@cmf.nrl.navy.mil>
2011-07-14 13:55:49 -0400

    Moving the plist stuff to a separate class

---
+++ osx/PrefPane/JCILaunchdPlist.h
@@ -0,0 +1,72 @@
+//
+//  JCILaunchdPlist.h
+//  Jenkins Preference Pane
+//
+//  Created by William Ross on 7/14/11.
+//  Copyright 2011 Will Ross. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <AvailabilityMacros.h>
+
+@interface JCILaunchdPlist : NSObject{
+@private
+	NSMutableDictionary *plist;
+}
+// launchd.plist values
+@property (nonatomic, retain, readwrite) NSString *label;
+@property (nonatomic, retain, readwrite) NSNumber *disabled;
+@property (nonatomic, retain, readwrite) NSString *userName;
+@property (nonatomic, retain, readwrite) NSString *groupName;
+@property (nonatomic, retain, readwrite) NSMutableDictionary *inetdCompatibility;
+@property (nonatomic, retain, readwrite) NSMutableArray *limitLoadToHosts;
+@property (nonatomic, retain, readwrite) NSMutableArray *limitLoadFromHosts;
+@property (nonatomic, retain, readwrite) NSString *limitLoadToSessionType;
+@property (nonatomic, retain, readwrite) NSString *program;
+@property (nonatomic, retain, readwrite) NSMutableArray *programArguments;
+@property (nonatomic, retain, readwrite) NSNumber *enableGlobbing;
+@property (nonatomic, retain, readwrite) NSNumber *enableTransactions;
+@property (nonatomic, retain, readwrite) NSNumber *onDemand;
+@property (nonatomic, retain, readwrite) id<NSObject> keepAlive;
+@property (nonatomic, retain, readwrite) NSNumber *runAtLoad;
+@property (nonatomic, retain, readwrite) NSString *rootDirectory;
+@property (nonatomic, retain, readwrite) NSString *workingDirectory;
+@property (nonatomic, retain, readwrite) NSMutableDictionary *environmentVariables;
+@property (nonatomic, retain, readwrite) NSNumber *umask;
+@property (nonatomic, retain, readwrite) NSNumber *timeOut;
+@property (nonatomic, retain, readwrite) NSNumber *exitTimeOut;
+@property (nonatomic, retain, readwrite) NSNumber *throttleInterval;
+@property (nonatomic, retain, readwrite) NSNumber *initGroups;
+@property (nonatomic, retain, readwrite) NSMutableArray *watchPaths;
+@property (nonatomic, retain, readwrite) NSMutableArray *queueDirectories;
+@property (nonatomic, retain, readwrite) NSNumber *startOnMount;
+@property (nonatomic, retain, readwrite) NSNumber *startIOnterval;
+@property (nonatomic, retain, readwrite) id<NSObject> startCalendarInterval;
+@property (nonatomic, retain, readwrite) NSString *standardInPath;
+@property (nonatomic, retain, readwrite) NSString *standardOutPath;
+@property (nonatomic, retain, readwrite) NSString *standardErrorPath;
+@property (nonatomic, retain, readwrite) NSNumber *debug;
+@property (nonatomic, retain, readwrite) NSNumber *waitForDebugger;
+@property (nonatomic, retain, readwrite) NSMutableDictionary *softResourceLimits;
+@property (nonatomic, retain, readwrite) NSMutableDictionary *hardResourceLimits;
+@property (nonatomic, retain, readwrite) NSNumber *nice;
+@property (nonatomic, retain, readwrite) NSNumber *abandonProcessGroup;
+@property (nonatomic, retain, readwrite) NSNumber *hopefullyExitsFirst;
+@property (nonatomic, retain, readwrite) NSNumber *hopefullyExitsLast;
+@property (nonatomic, retain, readwrite) NSNumber *lowPriorityIO;
+@property (nonatomic, retain, readwrite) NSNumber *launchOnlyOnce;
+@property (nonatomic, retain, readwrite) NSMutableDictionary *machServices;
+@property (nonatomic, retain, readwrite) id<NSObject> sockets;
+
+// Other stuff
+@property (nonatomic, retain, readonly) NSMutableDictionary *plist;
+@property (nonatomic, retain, readwrite) NSString *path;
+@property (nonatomic, assign, readwrite, getter=isRunning) BOOL running;
+
+-(id)initWithPath:(NSString *)plistPath;
+-(void)load;
+-(void)unload;
+-(void)start;
+-(void)stop;
+
+@end
---
+++ osx/PrefPane/JCILaunchdPlist.m
@@ -0,0 +1,290 @@
+//
+//  JCILaunchdPlist.m
+//  Jenkins Preference Pane
+//
+//  Created by William Ross on 7/14/11.
+//  Copyright 2011 Will Ross. All rights reserved.
+//
+
+#import "JCILaunchdPlist.h"
+#import <ctype.h>
+#import <objc/runtime.h>
+
+id plistGetProxy(id self, SEL selector);
+void plistSetProxy(id self, SEL selector, id value);
+static NSSet *propertySet = nil;
+
+@interface JCILaunchdPlist()
+@property (nonatomic, retain, readwrite) NSMutableDictionary *plist;
+-(void)read;
+-(void)save;
+@end
+
+@implementation JCILaunchdPlist
+
+@dynamic label;
+@dynamic disabled;
+@dynamic userName;
+@dynamic groupName;
+@dynamic inetdCompatibility;
+@dynamic limitLoadToHosts;
+@dynamic limitLoadFromHosts;
+@dynamic limitLoadToSessionType;
+@dynamic program;
+@dynamic programArguments;
+@dynamic enableGlobbing;
+@dynamic enableTransactions;
+@dynamic onDemand;
+@dynamic keepAlive;
+@dynamic runAtLoad;
+@dynamic rootDirectory;
+@dynamic workingDirectory;
+@dynamic environmentVariables;
+@dynamic umask;
+@dynamic timeOut;
+@dynamic exitTimeOut;
+@dynamic throttleInterval;
+@dynamic initGroups;
+@dynamic watchPaths;
+@dynamic queueDirectories;
+@dynamic startOnMount;
+@dynamic startIOnterval;
+@dynamic startCalendarInterval;
+@dynamic standardInPath;
+@dynamic standardOutPath;
+@dynamic standardErrorPath;
+@dynamic debug;
+@dynamic waitForDebugger;
+@dynamic softResourceLimits;
+@dynamic hardResourceLimits;
+@dynamic nice;
+@dynamic abandonProcessGroup;
+@dynamic hopefullyExitsFirst;
+@dynamic hopefullyExitsLast;
+@dynamic lowPriorityIO;
+@dynamic launchOnlyOnce;
+@dynamic machServices;
+@dynamic sockets;
+
+@synthesize plist;
+@synthesize path;
+
+-(id)initWithPath:(NSString *)plistPath{
+	if((self = [super init])){
+		self.path = plistPath;
+		NSFileManager *fm = [[NSFileManager alloc] init];
+		if([fm fileExistsAtPath:self.path]){
+			[self read];
+		}else{
+			self.plist = [NSMutableDictionary dictionary];
+		}
+		[fm release];
+	}
+	return self;
+}
+
+-(void)load{
+	// Reload and start the daemon
+	NSArray *loadArgs = [NSArray arrayWithObjects:@"load", self.path, nil];
+	NSArray *startArgs = [NSArray arrayWithObjects:@"start", self.path, nil];
+	[NSTask launchedTaskWithLaunchPath:@"/bin/launchctl" arguments:loadArgs];
+	[NSTask launchedTaskWithLaunchPath:@"/bin/launchctl" arguments:startArgs];
+}
+
+-(void)unload{
+	NSArray *unloadArgs = [NSArray arrayWithObjects:@"unload", self.path, nil];
+	[NSTask launchedTaskWithLaunchPath:@"/bin/launchctl" arguments:unloadArgs];
+}
+
+-(void)start{
+	
+}
+
+-(void)stop{
+	
+}
+
+-(void)read{
+	NSData *plistData = [NSData dataWithContentsOfFile:self.path];
+	self.plist = [NSPropertyListSerialization propertyListFromData:plistData mutabilityOption:NSPropertyListMutableContainersAndLeaves format:NULL errorDescription:NULL];
+}
+
+-(void)save{
+	
+}
+
+-(BOOL)isRunning{
+	// Get launchd listing (/bin/launchctl list)
+	NSTask *existsTask = [[NSTask alloc] init];
+	[existsTask setLaunchPath:@"/bin/launchctl"];
+	[existsTask setArguments:[NSArray arrayWithObjects:@"list", nil]];
+	NSPipe *existsPipe = [[NSPipe alloc] init];
+	[existsTask setStandardOutput:existsPipe];
+	[existsTask launch];
+	[existsTask waitUntilExit];
+	NSFileHandle *existsOutput = [existsPipe fileHandleForReading];
+	NSData *existsData = [[existsOutput readDataToEndOfFile] retain];
+	NSString *allLines = [[NSString alloc] initWithData:existsData encoding:NSUTF8StringEncoding];
+	NSArray *rawLines = [[allLines componentsSeparatedByCharactersInSet:[NSCharacterSet newlineCharacterSet]] retain];
+	[allLines release];
+	/*
+	 Search for the job. The format for each line is
+	 PID	ExitState	launchdLabel
+	 The space between each column is '\t'. If no value is given, the character '-' is substituted
+	 */
+	for(NSString *line in rawLines){
+		if([line isEqualToString:@""]){
+			continue;
+		}
+		NSArray *lineData = [line componentsSeparatedByString:@"\t"];
+		if([[lineData objectAtIndex:2] isEqualToString:@"org.jenkins-ci"]){
+			if([[lineData objectAtIndex:0] isEqualToString:@"-"]){
+				// Loaded, not running
+				return NO;
+			}else{
+				// Loaded and running
+				return YES;
+			}
+		}
+	}
+	// Not loaded
+	return NO;
+}
+
+-(void)setRunning:(BOOL)runJenkins{
+	if(self.running != runJenkins){
+		[self willChangeValueForKey:@"running"];
+		[self unload];
+		if(runJenkins){
+			[self load];
+		}
+		[self didChangeValueForKey:@"running"];
+	}
+}
+
++(BOOL)resolveInstanceMethod:(SEL)sel{
+	NSString *selector = NSStringFromSelector(sel);
+	if([propertySet containsObject:selector]){
+		if([selector rangeOfString:@"set"].location == NSNotFound){
+			// getter
+			return class_addMethod([self class], sel, (IMP)&plistGetProxy, "@:");
+		}else{
+			// setter
+			return class_addMethod([self class], sel, (IMP)&plistSetProxy, "@:@");
+		}
+	}else{
+		return NO;
+	}
+}
+
++(void)initialize{
+	propertySet = [[NSSet alloc] initWithObjects:@"label",
+												 @"disabled",
+												 @"userName",
+												 @"groupName",
+												 @"inetdCompatibility",
+												 @"limitLoadToHosts",
+												 @"limitLoadFromHosts",
+												 @"limitLoadToSessionType",
+												 @"program",
+												 @"programArguments",
+												 @"enableGlobbing",
+												 @"enableTransactions",
+												 @"onDemand",
+												 @"keepAlive",
+												 @"runAtLoad",
+												 @"rootDirectory",
+												 @"workingDirectory",
+												 @"environmentVariables",
+												 @"umask",
+												 @"timeOut",
+												 @"exitTimeOut",
+												 @"throttleInterval",
+												 @"initGroups",
+												 @"watchPaths",
+												 @"queueDirectories",
+												 @"startOnMount",
+												 @"startIOnterval",
+												 @"startCalendarInterval",
+												 @"standardInPath",
+												 @"standardOutPath",
+												 @"standardErrorPath",
+												 @"debug",
+												 @"waitForDebugger",
+												 @"softResourceLimits",
+												 @"hardResourceLimits",
+												 @"nice",
+												 @"abandonProcessGroup",
+												 @"hopefullyExitsFirst",
+												 @"hopefullyExitsLast",
+												 @"lowPriorityIO",
+												 @"launchOnlyOnce",
+												 @"machServices",
+												 @"sockets", nil];
+}
+
+@end
+
+id plistGetProxy(id self, SEL selector){
+	NSString *propertyName = [NSStringFromSelector(selector) capitalizedString];
+	id value = [[self plist] objectForKey:propertyName];
+	if(value == nil){
+		if([propertyName isEqualToString:@"Program"]){
+			if([self programArguments] != nil){
+				return [[self programArguments] objectAtIndex:0];
+			}
+		}else if([propertyName isEqualToString:@"OnDemand"]){
+			// Note: this key is deprecated, and is contrary to the 10.5 and 10.6 default
+			return [NSNumber numberWithBool:YES];
+		}else if([propertyName isEqualToString:@"KeepAlive"]){
+			return [NSNumber numberWithBool:NO];
+		}else if([propertyName isEqualToString:@"RunAtLoad"]){
+			return [NSNumber numberWithBool:NO];
+		}else if([propertyName isEqualToString:@"ExitTimeOut"]){
+			return [NSNumber numberWithInt:20];
+		}else if([propertyName isEqualToString:@"ThrottleInterval"]){
+			return [NSNumber numberWithInt:10];
+		}else if([propertyName isEqualToString:@"InitGroups"]){
+			return [NSNumber numberWithBool:YES];
+		}else if([propertyName isEqualToString:@"LaunchOnlyOnce"]){
+			// Inferred from the man page
+			return [NSNumber numberWithBool:NO];
+		}else if([propertyName isEqualToString:@"EnableGlobbing"]){
+			// Inferred from the man page
+			return [NSNumber numberWithBool:NO];
+		}else if([propertyName isEqualToString:@"EnableTransactions"]){
+			// Inferred from the man page
+			return [NSNumber numberWithBool:NO];
+		}else if([propertyName isEqualToString:@"StartOnMount"]){
+			// Inferred from the man page
+			return [NSNumber numberWithBool:NO];
+		}else if([propertyName isEqualToString:@"Debug"]){
+			// Inferred from the man page
+			return [NSNumber numberWithBool:NO];
+		}else if([propertyName isEqualToString:@"WaitForDebugger"]){
+			// Inferred from the man page
+			return [NSNumber numberWithBool:NO];
+		}else if([propertyName isEqualToString:@"AbandonProcessGroup"]){
+			// Inferred from the man page
+			return [NSNumber numberWithBool:NO];
+		}else if([propertyName isEqualToString:@"HopefullyExitsFirst"]){
+			// Inferred from the man page
+			return [NSNumber numberWithBool:NO];
+		}else if([propertyName isEqualToString:@"HopefullyExitsFirst"]){
+			// Inferred from the man page
+			return [NSNumber numberWithBool:NO];
+		}
+	}else{
+		return value;
+	}
+	return nil;
+}
+
+void plistSetProxy(id self, SEL selector, id value){
+	NSString *littleName = NSStringFromSelector(selector);
+	[self willChangeValueForKey:littleName];
+	NSString *propertyName = [littleName capitalizedString];
+	[[self plist] setValue:value forKey:propertyName];
+	[self didChangeValueForKey:littleName];
+	[self save];
+}
